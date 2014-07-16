332e0f5901a76ecdaa6f705f70815a1d871ba9d7
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-07-16 10:15:41 +0300

    Removed Correct Answere counter

--- app/scripts/controllers/all-questions.js
+++ app/scripts/controllers/all-questions.js
@@ -3,20 +3,21 @@ app.controller('AllQuestionsCtrl', function($rootScope, $scope, $routeParams, $l
 
   var model = $scope.model = {};
   var root = $scope.root;
-  var current = 0;
+  
   //var top = $scope.top;
 
   model.answers = {
     a: false,
     b: false,
-    c: false
+    c: false,
   };
 
   model.alert = false;
+  model.current = 0;
 
   data.GetQuestions({}).then(function (res) {
     model.questions = res;
-    model.question = model.questions[current];
+    model.question = model.questions[model.current];
 
   }, function (err) {
     
@@ -32,6 +33,7 @@ app.controller('AllQuestionsCtrl', function($rootScope, $scope, $routeParams, $l
   $scope.ValidateAnswer = function () {
     var correctAnswers = model.question.v.split(' ');
     var setAnswers = [];
+    
     angular.forEach(model.answers, function (value, key) {
       if (value) {
         setAnswers.push(key);
@@ -41,22 +43,29 @@ app.controller('AllQuestionsCtrl', function($rootScope, $scope, $routeParams, $l
     model.valid = angular.equals(setAnswers.sort(), correctAnswers.sort());
     model.alert = true;
 
+    
+    $timeout(function () {
+      
+      model.alert = false;
+      
+      $scope.NextQuestion();
+
+    }, 3000);
+    
   };
 
   $scope.NextQuestion = function () {
 
-    current = current + 1;
-    model.question = model.questions[current];
+    model.current = model.current + 1;
+    model.question = model.questions[model.current];
 
     $scope.ResetAnsweres();
   };
 
   $scope.ResetAnsweres = function () {
-    model.answers = {
-      a: false,
-      b: false,
-      c: false
-    };
+    model.answers.a = false;
+    model.answers.b = false;
+    model.answers.c = false;
   };
 
 });
