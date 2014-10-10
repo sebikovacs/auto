8fffd4cbad7d46fc91f603570b856109232f8024
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-10-10 15:42:04 +0300

    fixed jshint

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,27 +8,27 @@ app.controller('DashboardCtrl', function($rootScope, $scope, $routeParams, $loca
   data.GetQuestions().then(function (res) {
 
     model.a = {
-    	corect: taggedFilter(model.questions.a, 'corect').length,
-    	incorect: taggedFilter(model.questions.a, 'incorect').length,
-    	total: model.questions.a.length
+      corect: taggedFilter(model.questions.a, 'corect').length,
+      incorect: taggedFilter(model.questions.a, 'incorect').length,
+      total: model.questions.a.length
     };
 
     model.b = {
-    	corect: taggedFilter(model.questions.b, 'corect').length,
-    	incorect: taggedFilter(model.questions.b, 'incorect').length,
-    	total: model.questions.b.length
+      corect: taggedFilter(model.questions.b, 'corect').length,
+      incorect: taggedFilter(model.questions.b, 'incorect').length,
+      total: model.questions.b.length
     };
 
     model.c = {
-    	corect: taggedFilter(model.questions.c, 'corect').length,
-    	incorect: taggedFilter(model.questions.c, 'incorect').length,
-    	total: model.questions.c.length
+      corect: taggedFilter(model.questions.c, 'corect').length,
+      incorect: taggedFilter(model.questions.c, 'incorect').length,
+      total: model.questions.c.length
     };
 
     model.d = {
-    	corect: taggedFilter(model.questions.d, 'corect').length,
-    	incorect: taggedFilter(model.questions.d, 'incorect').length,
-    	total: model.questions.d.length
+      corect: taggedFilter(model.questions.d, 'corect').length,
+      incorect: taggedFilter(model.questions.d, 'incorect').length,
+      total: model.questions.d.length
     };
 
     
